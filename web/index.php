f6ff838d00048ce952fc81de1580a1ae635da6ca
sanernecro <sanernecro@gmail.com>
2016-11-02 23:12:45 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once("config.php");
-require_once("includes/functions.php");
+require_once("functions.php");
 
 function getIpAddress() {
     if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
