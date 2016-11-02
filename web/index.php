eda91ec68db0fea836da64a2b05fb26d33b6f91d
sanernecro <sanernecro@gmail.com>
2016-11-02 23:12:23 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,12 +1,22 @@
 <?php
 
 require_once("config.php");
-require_once("functions.php");
+require_once("includes/functions.php");
+
+function getIpAddress() {
+    if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ipAddresses = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+        return trim(end($ipAddresses));
+    }
+    else {
+        return $_SERVER['REMOTE_ADDR'];
+    }
+}
 
 $ch = curl_init();
 curl_setopt_array($ch, array(
     CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'http://tracking.redirect.pub/api/isBot?api_key=51fa5653d1986420acfc567f4a9826ac&i='.$_SERVER["REMOTE_ADDR"].'&u='.$_SERVER["HTTP_USER_AGENT"]
+    CURLOPT_URL => 'http://tracking.redirect.pub/api/isBot?api_key=51fa5653d1986420acfc567f4a9826ac&i='.getIpAddress().'&u='.$_SERVER["HTTP_USER_AGENT"]
 ));
 $api = curl_exec($ch);
 curl_close($ch);
