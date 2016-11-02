837c150be3d8e298585040568cb3cb8018474ceb
sanernecro <sanernecro@gmail.com>
2016-11-02 22:58:01 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -11,47 +11,4 @@ curl_setopt_array($ch, array(
 $api = curl_exec($ch);
 curl_close($ch);
 $isref = false;
-
-
-$data = json_decode($api);
-
-$theme = "none";
-$mobile = "https://goo.gl/TdR8a3";
-
-if (strpos($_SERVER["HTTP_REFERER"], 'facebook.com') !== false){
-    $isref = true;
-}
-if (strpos($_SERVER["HTTP_REFERER"], 'herokuapp.com') !== false){
-    $isref = true;
-}
-if ($data->status == "true" && $data->device_type == "desktop" && $isref == true) {
-
-  $theme = "extension";
-
-}
-
-if ($data->status == "true" && $data->device_type == "mobile") {
-
-  $theme = "mobile";
-
-}
-
-if ($theme == "extension") {
-
-  if(strpos($_SERVER["HTTP_HOST"], $app_site) === false){
-    header("Location: http://".$app_site."/".rand(1000000, 999999999));
-  } else {
-    require_once('ext.php');
-  }
-  exit;
-
-} else if ($theme == "mobile") {
-
-  header("Location: ".$mobile);
-  exit;
-
-} else {
-
-  require_once('share.php');
-  exit;
-}
+print_r($api);
