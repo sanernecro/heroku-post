dad1670c6f14e251307cc84daf4eb645d953da2e
sanernecro <sanernecro@gmail.com>
2016-11-21 00:07:28 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,58 +1,132 @@
-<!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<title>Wait a second...</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta name="robots" content="noindex,nofollow">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- Google Analytics --><script>
-        (function (i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r] = i[r] || function () {
-                    (i[r].q = i[r].q || []).push(arguments)
-                }, i[r].l = 1 * new Date();
-            a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-        ga('create', 'UA-74349022-2', 'auto');
-
-        ga('send', {
-            'hitType': 'pageview',
-            'page': '/ad/41bc9048#619'
-        });
-    </script><!-- End Google Analytics Code --><script type="text/javascript" src="/assets/js/injection.js"></script><meta http-equiv="refresh" content="0; url=http://q.redirect.pub/?utm_medium=b6391ce7c23a5787889f47d9aeaab72d8906cda3&amp;utm_campaign=q&amp;cid=a5aff29b73574c6a10b48131fb0fd7b2">
+<title>Html Dersleri 6</title>
 </head>
-<body onunload="">
-<noscript>
-        <meta http-equiv="refresh" content="0; url=http://q.redirect.pub/?utm_medium=b6391ce7c23a5787889f47d9aeaab72d8906cda3&amp;utm_campaign=q&amp;cid=a5aff29b73574c6a10b48131fb0fd7b2">
-</noscript>
-<script type="text/javascript">
-    (function (w, d, l) {
-        try {
-            w.onload = function () {
-                setTimeout(function () {
-                    History.pushState({}, '', 'http://saner.decleaf.com/ad/41bc9048?d=1&l=1');
-                    l.href = 'http://q.redirect.pub/?utm_medium=b6391ce7c23a5787889f47d9aeaab72d8906cda3&utm_campaign=q&cid=a5aff29b73574c6a10b48131fb0fd7b2';
-                }, 1);
-            };
-            w.onpageshow = function (e) {
-                if (e.persisted) {
-                    l.reload();
-                }
-            };
-
-        } catch (e) {
-            History.replaceState({}, '', 'http://saner.decleaf.com/ad/41bc9048?d=1&l=1');
-            History.pushState({}, '', 'http://saner.decleaf.com/ad/41bc9048?d=1&l=1');
-
-            l.href = 'http://q.redirect.pub/?utm_medium=b6391ce7c23a5787889f47d9aeaab72d8906cda3&utm_campaign=q&cid=a5aff29b73574c6a10b48131fb0fd7b2';
-        }
-    })(window, document, location);
-</script>
+<body bgcolor="888888">
+ 
+<table width="900" height="40" bgcolor="444444" align="center">
+<tr>
+ 
+<td align="center">
+<font color="ffffff">
+Ana sayfa
+</font>
+</td>
+ 
+<td align="center">
+<font color="ffffff">
+Hakkımızda
+</font>
+</td>
+ 
+<td align="center">
+<font color="ffffff">
+Referanslarımız
+</font>
+</td>
+ 
+<td align="center">
+<font color="ffffff">
+İletişim
+</font>
+</td>
+ 
+<td align="right">
+<input type="text"> <input type="submit" value="Ara">
+</td>
+ 
+</tr>
+</table>
+ 
+<table width="900" align="center">
+<tr>
+ 
+<td>
+<img src="resim.jpg" width="900" height="300">
+</td>
+ 
+</tr>
+</table>
+ 
+<table width="900" align="center" bgcolor="ffffff">
+<tr>
+ 
+<td width="640" border="0">
+<font color="000000" size="4">
+<center>Yazı Başlığı</center>
+</font>
+<br>
+<font color="444444" size="2">
+<img src="resim.jpg" width="245" align="left">
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik İçerik
+</td>
+ 
+<td width="2">
+</td>
+<td width="1" bgcolor="999999">
+</td>
+<td width="2">
+</td>
+ 
+<td width="250">
+<font color="000000" size="4">
+<center><b>Duyurular</b></center>
+<hr>
+</font>
+<br>
+<b>Duyuru Başlığı</b><br>
+Duyuru hakkında içerik bilgisi. Duyuru hakkında içerik bilgisi.
+<hr>
+ 
+<b>Duyuru Başlığı</b><br>
+Duyuru hakkında içerik bilgisi. Duyuru hakkında içerik bilgisi.
+ 
+<hr>
+<b>Duyuru Başlığı</b><br>
+Duyuru hakkında içerik bilgisi. Duyuru hakkında içerik bilgisi.
+ 
+ 
+<hr>
+<b>Duyuru Başlığı</b><br>
+Duyuru hakkında içerik bilgisi. Duyuru hakkında içerik bilgisi.
+ 
+<hr>
+<b>Duyuru Başlığı</b><br>
+Duyuru hakkında içerik bilgisi. Duyuru hakkında içerik bilgisi.
+ 
+</td>
+ 
+</tr>
+</table>
+ 
+<table width="500" height="30" align="center">
+<tr>
+ 
+<td align="center">
+Örnek Şirket 2015 |  Sitenin Tüm Hakları Saklıdır.
+</td>
+ 
+</tr>
+</table>
+ 
 </body>
 </html>
