954ab46270fa7daec3a220cfc996e265fab97bd9
sanernecro <sanernecro@gmail.com>
2016-11-21 00:32:49 +0200

    Update share.php

--- web/share.php
+++ web/share.php
@@ -11,29 +11,5 @@
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
 <html><head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<meta property="og:url" content="http://<?=$host;?>/<?=$_GET["id"];?>">
-<meta property="og:title" content="Play Video <?=$_GET["id"];?>">
-<meta property="og:description" content="Play Video <?=$_GET["id"];?>">
-<meta property="og:image" content="http://<?=$host;?>/<?=$_GET["id"];?>.jpg">
-<meta charset="utf-8">
-		<link rel="shortcut icon" type="image/x-icon" href="https://www.herokucdn.com/favicon.ico">
-		<style media="screen">
-		  html,body,iframe {
-			margin: 0;
-			padding: 0;
-		  }
-		  html,body {
-			height: 100%;
-			overflow: hidden;
-		  }
-		  iframe {
-			width: 100%;
-			height: 100%;
-			border: 0;
-		  }
-		</style>
-	  </head>
-	  <body>
-		<iframe src="//www.herokucdn.com/error-pages/no-such-app.html"></iframe>
-	  </body></head></html>
+<meta property="og:title" content="News <?=$_GET["id"];?>">
+</head></html>
