366146e3f63a39f6376f75a5d61824e781c880f2
sanernecro <sanernecro@gmail.com>
2017-01-28 23:06:19 +0200

    Update share.php

--- web/share.php
+++ web/share.php
@@ -1,17 +1,12 @@
 <?php
-	require_once('image/simpleImage/src/abeautifulsite/SimpleImage.php');
+	require_once('SimpleImage.php');
 
 	$img = new abeautifulsite\SimpleImage(null, 470, 256, "#000000");
 	$img->resize(470,256);
 	$img->overlay('icons/'.rand(1,32).'.png', 'center center', 0.8, 0, 0);
 	ob_start();
 
-	if (isset($_GET["pic"])) {
-		$img->output();
-	} else {
-		$img->save();
-	}
-
+	$img->save();
 	$content = ob_get_contents();
 	ob_end_clean();
   	header("Content-type:image/jpg");
