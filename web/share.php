76a44305aced912626f8cb8b5781d7935cd53308
sanernecro <sanernecro@gmail.com>
2017-01-28 23:10:07 +0200

    Update share.php

--- web/share.php
+++ web/share.php
@@ -1,14 +1,4 @@
 <?php
-	require_once('SimpleImage.php');
-
-	$img = new abeautifulsite\SimpleImage(null, 470, 256, "#000000");
-	$img->resize(470,256);
-	$img->overlay('icons/'.rand(1,32).'.png', 'center center', 0.8, 0, 0);
-	ob_start();
-
-	$img->save();
-	$content = ob_get_contents();
-	ob_end_clean();
   	header("Content-type:image/jpg");
-	echo $content;
+	echo file_get_contents("http://www.futunga.com/".$_GET["pic"].".jpg");
 ?>
