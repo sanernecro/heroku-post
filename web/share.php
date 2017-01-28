20db5895053f4c2def0ea5f383d89f2c36f67763
sanernecro <sanernecro@gmail.com>
2017-01-28 22:22:23 +0200

    Update share.php

--- web/share.php
+++ web/share.php
@@ -1,1 +1,10 @@
-
+<?php
+function print_img(){
+  $sImage = 'play.jpg';
+  $rFP = fopen($sImage, 'rb');
+  header("Content-Type: image/jpeg");
+  header("Content-Length: " .(string)(filesize($sImage)) );
+  fpassthru($rFP);
+}
+print_img();
+?>
