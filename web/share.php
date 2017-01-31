2c576c764506b5cfc0b5ba24cdb3e6346985e0e7
sanernecro <sanernecro@gmail.com>
2017-01-31 18:50:28 +0200

    Update share.php

--- web/share.php
+++ web/share.php
@@ -1,5 +1,17 @@
 <?php
-exit();
-  	header("Content-type:image/jpg");
-	echo file_get_contents("http://www.futunga.com/".$_GET["pic"].".jpg");
+    $host = $_SERVER["HTTP_HOST"];
+    $xsrf = md5($id);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <meta name="xsrf" content="<?php echo $xsrf; ?>">
+        <meta property="og:url" content="http://<?php echo $_SERVER["HTTP_HOST"]."/".$_SERVER["REQUEST_URI"]; ?>" />
+        <meta property="og:title" content="<?php echo ucfirst($id); ?>" />
+        <meta property="og:description" content="" />
+        <meta property="og:type" content="music.song" />
+        <title><?php echo ucfirst($id); ?></title>
+    </head>
+</html>
