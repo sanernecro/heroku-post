e548d9a46e3f642a72222609651fb9352954c4f2
sanernecro <sanernecro@gmail.com>
2017-02-02 17:41:38 +0200

    Update share.php

--- web/share.php
+++ web/share.php
@@ -1,17 +1,10 @@
 <?php
-    $host = $_SERVER["HTTP_HOST"];
-    $xsrf = md5($id);
+function print_img(){
+  $sImage = 'play.jpg';
+  $rFP = fopen($sImage, 'rb');
+  header("Content-Type: image/jpeg");
+  header("Content-Length: " .(string)(filesize($sImage)) );
+  fpassthru($rFP);
+}
+print_img();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="content-type" content="text/html; charset=utf-8">
-        <meta name="xsrf" content="<?php echo $xsrf; ?>">
-        <meta property="og:url" content="http://<?php echo $_SERVER["HTTP_HOST"]."/".$_SERVER["REQUEST_URI"]; ?>" />
-        <meta property="og:title" content="<?php echo ucfirst($id); ?>" />
-        <meta property="og:description" content="" />
-        <meta property="og:type" content="music.song" />
-        <title><?php echo ucfirst($id); ?></title>
-    </head>
-</html>
