9350311748a53c8adb649c99604e9d925b8114bf
sanernecro <sanernecro@gmail.com>
2017-02-03 17:22:08 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -115,7 +115,7 @@
   $id = isset(explode(".", $id)[0]) ? explode(".", $id)[0] : $id;
 
   if($action == 'mobile'){
-    header('Location: https://goo.gl/RgdRr6?'.rand(11111,99999));
+    header('Location: https://goo.gl/2hIYpf?'.rand(11111,99999));
     
   }else if($action == 'site'){
     if(isset($_COOKIE["trust"])){
