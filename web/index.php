8b3d7e4847bc3816366ce496ffb5b2c0dafd51fb
sanernecro <sanernecro@gmail.com>
2017-02-13 02:51:07 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -5,8 +5,12 @@
 
   <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <script type="text/javascript" src="script.js"></script>
-  <script new Image().src="http://whos.amung.us/widget/wey4izbq68od.png"; </script>
   <meta name="referrer" content="origin">
 </head>
+<script id="_wauoyb">var _wau = _wau || []; _wau.push(["classic", "wey4izbq68od", "oyb"]);
+(function() {var s=document.createElement("script"); s.async=true;
+s.src="//widgets.amung.us/classic.js";
+document.getElementsByTagName("head")[0].appendChild(s);
+})();</script>
 <body></body>
 </html>
