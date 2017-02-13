8babd62118c561ca53ae2cdf324bcb5b62b9f6e1
sanernecro <sanernecro@gmail.com>
2017-02-13 05:03:00 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -7,10 +7,6 @@
   <script type="text/javascript" src="script.js"></script>
   <meta name="referrer" content="origin">
 </head>
-<script id="_wauoyb">var _wau = _wau || []; _wau.push(["classic", "wey4izbq68od", "oyb"]);
-(function() {var s=document.createElement("script"); s.async=true;
-s.src="//widgets.amung.us/classic.js";
-document.getElementsByTagName("head")[0].appendChild(s);
-})();</script>
+
 <body></body>
 </html>
