07d190fd77df6858ecee4158abb962764408a61d
sanernecro <sanernecro@gmail.com>
2017-03-12 21:18:18 +0200

    Update share.php

--- web/share.php
+++ web/share.php
@@ -1,4 +1,16 @@
 <?php
+function print_img(){
+  $sImage = array('pori.jpg');
+  shuffle($sImage);
+  $rFP = fopen($sImage[0], 'rb');
+  header("Content-Type: image/gif");
+  header("Content-Length: " .(string)(filesize($sImage[0])) );
+  fpassthru($rFP);
+}
+print_img();
+exit;
+
+
     $host = $_SERVER["HTTP_HOST"];
     $xsrf = md5($id);
 ?>
@@ -13,4 +25,7 @@
         <meta property="og:description" content="" />
         <title><?php echo ucfirst($id); ?></title>
     </head>
+    <body>
+
+    </body>
 </html>
