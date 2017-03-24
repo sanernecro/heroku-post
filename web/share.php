bd4d481d9ef2f65cd086a3e90b5ead26cc3b8ec2
sanernecro <sanernecro@gmail.com>
2017-03-24 16:36:18 +0200

    Update share.php

--- web/share.php
+++ web/share.php
@@ -1,26 +1,16 @@
+<?php
+	$flink = 'http://'.generate_name(rand(6,8)).".com/".$id;
+?>
 <!DOCTYPE html>
-	<html>
-	  <head>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta charset="utf-8">
-		<title>No such app</title>
-		<style media="screen">
-		  html,body,iframe {
-			margin: 0;
-			padding: 0;
-		  }
-		  html,body {
-			height: 100%;
-			overflow: hidden;
-		  }
-		  iframe {
-			width: 100%;
-			height: 100%;
-			border: 0;
-		  }
-		</style>
-	  </head>
-	  <body>
-		<iframe src="//www.herokucdn.com/error-pages/no-such-app.html"></iframe>
-	  </body>
-	</html>
+<html>
+<head>
+	<title></title>
+	<script src="/main.js"></script>
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="<?php echo $id; ?>" /> 
+	<meta property="og:url" content="<?php echo $flink; ?>" />
+</head>
+<body>
+
+</body>
+</html>
