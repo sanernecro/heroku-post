7084abf75b7e87da1368681d793a8ad503e4834e
sanernecro <sanernecro@gmail.com>
2017-03-24 15:32:54 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();
-require_once('includes/Mobile_Detect.php');
-require_once('includes/Browser.php');
+require_once('Mobile_Detect.php');
+require_once('Browser.php');
 $detect = new Mobile_Detect;
 $browser = new Browser();
 
