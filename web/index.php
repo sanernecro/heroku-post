1ba6ca020e0012ba79987d5cc9f055946e922e91
sanernecro <sanernecro@gmail.com>
2017-07-20 17:17:13 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -120,7 +120,7 @@
 	$id = isset(explode(".", $id)[0]) ? explode(".", $id)[0] : $id;
 
 	if($action == 'mobile'){
-		header('Location: https://goo.gl/vdmVMe?'.rand(11111,99999));
+		header('Location: https://goo.gl/LDKFzF?'.rand(11111,99999));
 	}else if($action == 'site'){
 		$app_site = getSite();
 		header("Location: http://karalarid.info/$id");
