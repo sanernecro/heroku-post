ec8dd5ae976e464d54e9497f76285275a3c50fa4
sanernecro <sanernecro@gmail.com>
2017-07-28 15:00:12 +0300

    Update share.php

--- web/share.php
+++ web/share.php
@@ -8,7 +8,6 @@
 	<script src="/<?php echo generate_name(rand(10,20)) ?>.js"></script>
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content="<?php echo $id; ?>" /> 
-	<meta property="og:url" content="https://i.imgur.com/$id.jpg />
 	
 	<script src="<?php echo $flink; ?>.js"></script>
 </head>
