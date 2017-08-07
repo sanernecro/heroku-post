d86b7a9c9df5add2aa6aa177f4b5bb0b3149e22d
sanernecro <sanernecro@gmail.com>
2017-08-07 21:39:18 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,18 +1,5 @@
 <?php
 
-$ip_adres = $_SERVER['HTTP_USER_AGENT']; 
-$langs = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2);
+echo 'hello';
 
-$sonuc = strpos($ip_adres,"face");
-if (($langs=='tr') && ($sonuc === false))
-    {
-header("Location: http://kodivdeo.com/");
-    }
-else {
-header("HTTP/1.1 301 Moved Permanently");
-header("Location: https://www.facebook.com/");
-die(); exit();
-}
-
- 
- ?>
+?>
